424d2a02907df202500a9fe5603d129926592b8c
Eduarda Santos <mesps@cin.ufpe.br>
2024-03-02 13:08:44 -0300

    Adicionando componente de carrossel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import { Sometype_Mono } from "next/font/google";
 import styles from "./page.module.css";
 import Map from "@/components/Map";
 import Menu from "@/components/Menu";
-
-
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Menu />
+        <Carousel />
         <Map />
         <h1>asdasd</h1>
       </main>
