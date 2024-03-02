07d83213a92eea70af6711fa40a351ccc72c3b5f
Eduarda Santos <mesps@cin.ufpe.br>
2024-03-02 13:35:34 -0300

    Ajustaestilo do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,10 @@ import styles from "./styles.module.css";
 export default function Footer () {
   return (
     <div className={styles.container}>
-      <img src="/draws/footer.svg" />
+      <img
+        src="/draws/footer.svg"
+        className={styles.draw}
+      />
       <div className={styles['container-text']}>
         Projeto realizado para a disciplina Narrativas Inovadoras com Dados na Comunicação Digital - em colaboração com os cursos de Computação, Design e Jornalismo da UFPE.
       </div>
