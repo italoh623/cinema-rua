1b6f10b6425d239e85c26333d715339ea4cb201b
Ítalo Henrique <italohenrique014@gmail.com>
2024-03-03 21:59:48 -0300

    Corrigindo erro de importação do componente do Mapa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-import Map from "@/components/Map";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("@/components/Map"), {
+  loading: () => <div>"Loading..."</div>,
+  ssr: false,
+})
 import Menu from "@/components/Menu";
 import SessionImportance from "@/components/SessionImportance";
 import Carousel from "@/components/Carousel";
@@ -8,6 +13,7 @@ import Footer from "@/components/Footer";
 
 import styles from "./page.module.css";
 import SessionAppreciation from "@/components/SessionAppreciation";
+
 
 
 export default function Home() {
